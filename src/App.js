ab7806b268906086c898173c95393a7f68137e75
Meiqiao <chenmeiqiao940926@gmail.com>
2023-01-11 14:55:37 +1300

    add filter, fact and form section in React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,108 @@
+import "./style.css";
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+
 
 function App() {
-  return <h1>Hello World</h1>
+  const appTitle = "Today I learned"
+  return (
+    <>
+      {/* HEADER */}
+    <header className="header">
+    <div className="logo">
+      <img src="logo.png" alt="Today I learned logo" />
+      <h1>{appTitle}</h1>
+    </div>
+    <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+    </main>
+    </>
+  )
+}
+
+function NewFactForm() {
+  return <form className = "fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList() {
+  const facts = initialFacts;
+  return <section><ul className="facts-list">{facts.map((fact) => <Fact key={fact.id} fact={fact} />)}</ul>
+    <p>There are {facts.length} facts in the database</p>
+  </section>;
+}
+
+function Fact({fact}) {
+  return <li className="fact">
+  <p>
+    {fact.text}
+    <a
+      className="source"
+      href={fact.source}
+      target="_blank" rel="noreferrer"
+      >(Source)</a
+    >
+  </p>
+  <span className="tag" style={{backgroundColor: CATEGORIES.find((category)=>category.name===fact.category).color}}
+    >{fact.category}</span
+  >
+  <div className="vote-buttons">
+    <button>👍 {fact.votesInteresting}</button>
+    <button>🤯 {fact.votesMindblowing}</button>
+    <button>⛔ {fact.votesFalse}</button>
+  </div>
+</li>
 }
 
 export default App;
